34a4695301add8c9e9699b7b74ce0ce1be8998bc
Roberto Morales <roberto@morales.com.co>
2022-05-16 19:50:42 -0500

    Creación PruebaProps

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 
 import './App.css';
 import Formulario from './components/Formulario'
+import PruebaPropiedades from './components/PruebaPropiedades';
 
 function App() {
   return (
     <>
-      
-        
-
-        <Formulario/>
-      
-
+        <PruebaPropiedades cantidad= {9}/>
     </>
   );
 }
